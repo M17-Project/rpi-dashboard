7bcd221f268e9bd22d4a52e94a793b11c04e8c47
Wojciech Kaczmarski <w.kaczmarski@teletra.pl>
2024-01-26 10:40:04 +0100

    updated config.txt

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html>
+<?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
 <head>
   <link rel="stylesheet" href="style.css">
   <title>RRU Dashboard</title>
