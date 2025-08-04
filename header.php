b14a7ca089f429cb1f3dacf51f5f1804e45fbe4f
DK1MI <github@qrz.is>
2025-08-04 11:46:02 +0200

    renamed config php files

--- header.php
+++ header.php
@@ -12,9 +12,8 @@ header("Pragma: no-cache");
 <div class="header">
 <ul>
   <li><a class="active" href="index.php">Dashboard</a></li>
-  <li><a href="map.php">User Map</a></li>
-  <li><a href="gateway_config.php">Gateway Config</a></li>
-  <li><a href="admin.php">Admin</a></li>
+  <li><a href="config_gateway.php">Gateway Config</a></li>
+  <li><a href="config_dashboard.php">Dashboard Config</a></li>
   <li><a href="https://m17project.org" target=”_blank”>About</a></li>
   <li><a href="https://www.paypal.com/donate/?hosted_button_id=4HTHZCS8UYPU6" target=”_blank”>Donate</a></li>
 </ul>
